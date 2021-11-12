e5088366ff6ea0460a60fc784a7419c6edbd04db
Govind Kumar <govindsingh6498@gmail.com>
2021-11-12 22:55:09 +0530

    Added:Routes to payroll form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import PayrollForm from './components/payroll-form/PayrollForm';
+import {
+  BrowserRouter as Router,
+    Routes, 
+    Route
+  }from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Routes>
+        <Route path="/" component={PayrollForm} exact />
+        </Routes>
+      </Router>
     </div>
+    
   );
 }
 
